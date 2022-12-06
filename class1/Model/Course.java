b0e716c614605896dde594d72fd7b2fbc88a2db1
MyeongHyeonYoo <victorea5@naver.com>
2022-12-06 21:47:36 +0900

    uml_data 1

---
+++ class1/Model/Course.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Course {
+
+    /**
+     * Default constructor
+     */
+    public Course() {
+    }
+
+    /**
+     * 
+     */
+    private String id;
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private Interger numOfStudents;
+
+    /**
+     * @param student 
+     * @return
+     */
+    public void addStudent(Student student) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param id 
+     * @return
+     */
+    public void deleteStudent(Integer id) {
+        // TODO implement here
+        return null;
+    }
+
+}
